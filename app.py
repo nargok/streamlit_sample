16741b74235fe52d16dbc04857bf325e76f4793e
nargok <nargok@gmail.com>
2022-05-22 11:40:19 +0900

    title markdown

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write('Demo app')
+st.title('Sample App')
+
+st.markdown("## 見出し2")
+st.markdown("### 見出し3")
+
+st.markdown("""
+- task1
+- task2
+""")
+
+st.code('a = 123')

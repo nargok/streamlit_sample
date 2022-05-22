a70441427c993c9632ff70bdc19b77ef01a1dda6
nargok <nargok@gmail.com>
2022-05-22 13:11:34 +0900

    layout

--- app.py
+++ app.py
@@ -7,13 +7,23 @@ st.title("Sample App")
 if st.button("Click Me"):
     st.write("Clicked")
 
-options = st.multiselect(
+# options = st.multiselect(
+#     'What are you favorite colors?',
+#     ['Green', 'Yellow', 'Red', 'Blue'],
+#     ['Red']
+# )
+
+# st.write(f'選択肢: {options}')
+
+number = st.sidebar.slider('pick a number', 0, 100)
+st.sidebar.write(f'number: {number}')
+
+left_col, right_col = st.columns(2)
+
+options = left_col.multiselect(
     'What are you favorite colors?',
     ['Green', 'Yellow', 'Red', 'Blue'],
     ['Red']
 )
 
-st.write(f'選択肢: {options}')
-
-number = st.slider('pick a number', 0, 100)
-st.write(f'number: {number}')
+right_col.write(f'選択肢: {options}')
